f10577ad9867fc79582001cfb1d6fcbe9e47156d
EkaterinaKir <kittinwork@gmail.com>
2021-02-06 13:10:40 +0300

    add task_6

---
+++ Task_6/Task_6.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''
+Task 6
+Сгенерируйте и нарисуйте коврик Серпинского (https://en.wikipedia.org/wiki/Sierpinski_carpet)
+'''
+
+total_times = 5
+
+
+def sierpinski_carpet(total):
+    m = np.array([[0]])
+
+    for x in range(1, total + 1):
+        m = np.concatenate((np.concatenate((m, m, m), axis=1),
+                            np.concatenate((m, np.ones([3 ** (x - 1), 3 ** (x - 1)]), m), axis=1),
+                            np.concatenate((m, m, m), axis=1)), axis=0)
+    return m
+
+
+plt.spy(sierpinski_carpet(total_times))  # visualize non-zero values
+plt.title("Sierpiński carpet")
+plt.show()
