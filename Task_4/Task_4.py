547715b51213fcb124014a1f447c1083ebe97d47
EkaterinaKir <kittinwork@gmail.com>
2021-04-22 14:10:56 +0300

    fix

--- Task_4/Task_4.py
+++ Task_4/Task_4.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from random import randint
+
 
 '''
 Task 4
@@ -7,58 +9,21 @@ Task 4
 hint (https://www.youtube.com/watch?v=8ZSlT70pU7A&feature=emb_logo)
 '''
 
-point_a = np.array([0, 150])
-point_b = np.array([-200, -150])
-point_c = np.array([200, -150])
 
-start_point = [np.random.randint(-80, 80), np.random.randint(-80, 80)]
+def half_dist(p, q):
+    return [0.5 * (p[0] + q[0]), 0.5 * (p[1] + q[1])]
 
-plt.plot(point_a[0], point_a[1], marker='.', markersize=3)
-plt.plot(point_b[0], point_b[1], marker='.', markersize=3)
-plt.plot(point_c[0], point_c[1], marker='.', markersize=3)
 
-iterations = 0
-last_point = 0
+corner = [(0, 0), (0.5, 0.5), (1, 0)]
 
-while iterations <= 10000:
-    choice = np.random.choice([1, 2, 3])
+points = 10000
+x = np.zeros(points)
+y = np.zeros(points)
 
-    if iterations == 0:
-        if choice == 1:
-            new_point = np.array([((point_a[0] + start_point[0]) / 2), ((point_a[1] + start_point[1]) / 2)])
-            plt.plot(new_point[0], new_point[1], marker='.', markersize=3)
-            last_point = new_point
+for i in range(1, points):
+    vertex = randint(0, 2)
+    x[i], y[i] = half_dist(corner[vertex], (x[i - 1], y[i - 1]))
 
-        elif choice == 2:
-            new_point = np.array([((point_b[0] + start_point[0]) / 2), ((point_b[1] + start_point[1]) / 2)])
-            plt.plot(new_point[0], new_point[1], marker='.', markersize=3)
-            last_point = new_point
-
-        else:
-            new_point = np.array([((point_c[0] + start_point[0]) / 2), ((point_c[1] + start_point[1]) / 2)])
-            plt.plot(new_point[0], new_point[1], marker='.', markersize=3)
-            last_point = new_point
-
-        iterations += 1
-        continue
-
-    if choice == 1:
-        new_point = np.array([((point_a[0] + last_point[0]) / 2), ((point_a[1] + last_point[1]) / 2)])
-        plt.plot(new_point[0], new_point[1], marker='.', markersize=3)
-        last_point = new_point
-        iterations += 1
-
-    if choice == 2:
-        new_point = np.array([((point_b[0] + last_point[0]) / 2), ((point_b[1] + last_point[1]) / 2)])
-        plt.plot(new_point[0], new_point[1], marker='.', markersize=3)
-        last_point = new_point
-        iterations += 1
-
-    if choice == 3:
-        new_point = np.array([((point_c[0] + last_point[0]) / 2), ((point_c[1] + last_point[1]) / 2)])
-        plt.plot(new_point[0], new_point[1], marker='.', markersize=3)
-        last_point = new_point
-        iterations += 1
-
+plt.scatter(x, y, s=3)
 plt.title("Sierpiński triangle")
 plt.show()
